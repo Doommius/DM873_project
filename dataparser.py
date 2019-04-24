62440c62e0ce6629d66003deb3b95e031896f22f
Mark Jervelund <Mark@Jervelund.com>
2019-04-24 23:48:19 +0200

    did stuff

---
+++ dataparser.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+data_set = pd.read_csv("dataset/butterflies.txt", sep='\t')
+
+print (data_set)
--- sodeep.py
+++ sodeep.py
@@ -2,52 +2,74 @@ from keras import models
 from keras import layers
 from keras import optimizers
 
+import pandas as pd
+
 
 #
 #
 #  Idea 1,
 #  Parse the CSV file with all the information add the pictures to a array or data structure and fetch them from there?
 #
-#  Idea 2.
-#  Make lots of folders and do it that way?
-#
-#  Idea 3.
-#  TBD.
 #
 class datafetcher():
-        def __init__(self, folder):
-            self.folder = folder
+    __data_set = ""
 
-        def get_family(self, family):
-            return 0;
+    def __init__(self, folder):
+        self.folder = folder
+        # load data into type
+        self.__data_set = pd.read_csv("dataset/butterflies.txt", sep='\t')
 
+    def get_family(family):
+        return dataframe[self.__data_set['family'] == family]
 
-        def get_subfamily(self, family):
-            return 0;
+    def get_subfamily(subfamily):
+        return dataframe[self.__data_set['subfamily'] == subfamily]
 
+    def get_speices(species):
+        return dataframe[self.__data_set['species'] == species]
 
-        def get_speices(self, family):
-            return 0;
+    def get_genus(genus):
+        return dataframe[self.__data_set['genus'] == genus]
 
-
-        def get_genus(self, family):
-            return 0;
 
 class MyModel(keras.model):
-
     def __init__(self):
         super(MyModel, self).__init___()
         self.dense1 = layers.Dense(20, actication='relu')
         self.dense2 = layers.Dense(20, actication='relu')
         self.dense3 = layers.Dense(10, actication='softmax')
 
-    def call(self,inputs):
+    def call(self, dataframe):
+
+        train_dataframe = dataframe.sample(frac=0.8, random_state=200)
+        validation_dataframe = dataframe.drop(train.index)
+
+        train_datagen = ImageDataGenerator(
+            rescale=1. / 255,
+            shear_range=0.2,
+            zoom_range=0.2,
+            horizontal_flip=True)
+
+        test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+        train_generator = train_datagen.flow_from_dataframe(train_dataframe)
+
+        validation_generator = test_datagen.flow_from_dataframe(validation_dataframe)
+
+        model.fit_generator(
+            train_generator,
+            steps_per_epoch=2000,
+            epochs=50,
+            validation_data=validation_generator,
+            validation_steps=800)
+
+
         x = self.dense1(inputs)
         x = self.dense2(x)
         x = self.dense3(x)
         return x
 
 
-
 model = MyModel
+model.call(datafetcher.get_family(2))
 model.fit()
