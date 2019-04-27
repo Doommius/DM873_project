46ec6ad2f7f5a6152d98692ae4bb43a4fc193f84
Mark Jervelund <mark@jervelund.com>
2019-04-27 03:50:20 +0200

    fixed stuff.

--- sodeep.py
+++ sodeep.py
@@ -19,6 +19,8 @@ from keras.layers import Dropout, Flatten, Dense
 #  Parse the CSV file with all the information add the pictures to a array or data structure and fetch them from there?
 #
 #
+from pandas import DataFrame
+
 
 def get_subfamily(datagrame, subfamily):
     return dataframe(['subfamily' == 3])
@@ -39,65 +41,54 @@ class MyModel(models.Model):
         self.dense2 = layers.Dense(20, actication='relu')
         self.dense3 = layers.Dense(10, actication='softmax')
 
-    def call(self, dataframe):
 
-
-        model.compile(loss='binary_crossentropy',
-                      optimizer=optimizers.SGD(lr=1e-4, momentum=0.9),
-                      metrics=['accuracy'])
-
-
-        train_dataframe = pd.DataFrame.sample(dataframe, frac=0.8)
-        validation_dataframe = pd.DataFrame.drop(dataframe, train_dataframe.index)
-
-        train_datagen = pr.image.ImageDataGenerator(
-            rescale=1. / 255,
-            shear_range=0.2,
-            zoom_range=0.2,
-            horizontal_flip=True)
-
-        test_datagen = pr.image.ImageDataGenerator(rescale=1. / 255)
-
-        train_generator = train_datagen.flow_from_dataframe(train_dataframe,x_col='filename', y_col="family")
-
-        validation_generator = test_datagen.flow_from_dataframe(validation_dataframe,x_col='filename', y_col="family")
-
-        model.fit_generator(
-            train_generator,
-            samples_per_epoch=50,
-            epochs=5,
-            validation_data=validation_generator,
-            nb_val_samples=50)
-
-        x = self.dense1(input)
-        x = self.dense2(x)
-        x = self.dense3(x)
-        return x
-
-
-
-#config
+# config
 #   pd.set_option('display.max_rows', 50)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 
-
 dataframe = pd.read_csv("dataset/butterflies.txt", sep='\t')
+df: DataFrame = dataframe.drop(columns=['species', 'genus', 'subfamily'])
 
+df.loc[df['family'].isin([1]), 'family'] = "Papilionidae"
+df.loc[df['family'].isin([2]), 'family'] = "Pieridae"
+df.loc[df['family'].isin([3]), 'family'] = "Nymphalidae"
+df.loc[df['family'].isin([4]), 'family'] = "Lycaenidae"
+df.loc[df['family'].isin([5]), 'family'] = "Hesperiidae"
 
-df = dataframe.drop(columns=['species', 'genus','subfamily'])
-df = df.loc[df['family'].isin([1, 2])]
+df = df.loc[df['family'].isin(["Pieridae", "Papilionidae"])]
 print(df)
 
 model = applications.VGG16(weights='imagenet', include_top=False)
 
-top_model = Sequential()
-top_model.add(Flatten(input_shape=model.output_shape[1:]))
-top_model.add(Dense(256, activation='relu'))
-top_model.add(Dropout(0.5))
-top_model.add(Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy',
+              optimizer=optimizers.SGD(lr=1e-4, momentum=0.9),
+              metrics=['accuracy'])
 
+train_dataframe = pd.DataFrame.sample(df, frac=0.8)
+validation_dataframe = pd.DataFrame.drop(df, train_dataframe.index)
 
+train_datagen = pr.image.ImageDataGenerator(
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
 
-model.call(model, df[df.family == 3])
+test_datagen = pr.image.ImageDataGenerator(rescale=1. / 255)
+
+train_generator = train_datagen.flow_from_dataframe(train_dataframe, directory="dataset/base_set", x_col='filename',
+                                                    y_col="family", class_mode="categorical", target_size=(32, 32),
+                                                    batch_size=32)
+
+validation_generator = test_datagen.flow_from_dataframe(validation_dataframe, directory="dataset/base_set",
+                                                        x_col='filename', y_col="family", class_mode="categorical",
+                                                        target_size=(32, 32), batch_size=32)
+
+model.fit_generator(
+    train_generator,
+    samples_per_epoch=50,
+    epochs=5,
+    validation_data=validation_generator,
+    nb_val_samples=50)
+
 model.fit()
