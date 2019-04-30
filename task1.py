a900984468095e42cc84b11df040f2ae12437c71
Ilona Benko <ibenko@foi.hr>
2019-04-30 16:48:49 +0200

    task1, resolution, layers

--- task1.py
+++ task1.py
@@ -36,9 +36,9 @@ print(df)
 
 
 model = Sequential()
-model.add(Conv2D(64, (3, 3), activation="relu", input_shape=(32, 32, 3), padding='same'))
+model.add(Conv2D(64, (3, 3), activation="relu", input_shape=(224, 224, 3), padding='same'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(32, 3, 3))
+model.add(Conv2D(224, 3, 3))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Conv2D(64, 3, 3))
@@ -67,18 +67,18 @@ train_datagen = pr.image.ImageDataGenerator(
 test_datagen = pr.image.ImageDataGenerator(rescale=1. / 255)
 
 train_generator = train_datagen.flow_from_dataframe(train_dataframe, directory="dataset/base_set", x_col='filename',
-                                                    y_col="family", class_mode="categorical", target_size=(32, 32),
+                                                    y_col="family", class_mode="categorical", target_size=(224, 224),
                                                     batch_size=10)
 
 validation_generator = test_datagen.flow_from_dataframe(validation_dataframe, directory="dataset/base_set",
                                                         x_col='filename', y_col="family", class_mode="categorical",
-                                                        target_size=(32, 32), batch_size=10)
+                                                        target_size=(224, 224), batch_size=10)
 
 model.fit_generator(
     train_generator,
-    samples_per_epoch=50,
+    samples_per_epoch=30,
     epochs=20,
     validation_data=validation_generator,
-    nb_val_samples=50)
+    nb_val_samples=400)
 
 model.save_weights("task1.h5")
