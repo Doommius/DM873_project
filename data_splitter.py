889ca594d94ea7f1ec13f1f2b4e7b4295b1a457d
Mark Jervelund <mark@jervelund.com>
2019-05-14 17:58:33 +0200

    Cleaned up the splitter file and added some comments.

--- data_splitter.py
+++ data_splitter.py
@@ -1,28 +1,30 @@
+'''
+ Created by PyCharm.
+ User: Mark Jervelund <mark@jervelund.com>
+ Date: 14/05/19
+ Time: 4:46 PM
+'''
+
 import pandas as pd
 from pandas import DataFrame
 import shutil
 import os
 
-def get_subfamily(dataframe, subfamily):
-    return dataframe(['subfamily' == 3])
+# This is a simple script that splits the data into folders as required per the first assignment.
+# This is not he correct way of doing it as it should be handle in software via dataframes and not via the file structure as this assignment requires.
 
-
-def get_species(dataframe, species):
-    return dataframe(['species' == 3])
-
-
-def get_genus(dataframe, genus):
-    return dataframe(['genus' == 3])
-
-
+## BEGIN unused code
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 
 dataframe = pd.read_csv("dataset/butterflies.txt", sep='\t')
+#drop unused collums.
 df = dataframe.drop(columns=['species', 'genus', 'subfamily'])
+## END unused code
+
 
 #task 1
-task = "task1/"
+task = "task1/a/"
 # Delete old files
 if os.path.exists(task) and os.path.isdir(task):
     shutil.rmtree(task)
@@ -49,7 +51,7 @@ for sample in samples:
         shutil.copyfile("dataset/base_set/"+row[1], task+"validate/"+sample+"/"+(row[1].replace("/","_")))
 
 #task 2
-task = "task2/"
+task = "task2/b/"
 samples = ["Nymphalidae", "Lycaenidae"]
 
 # Delete old files
@@ -76,7 +78,7 @@ for sample in samples:
         shutil.copyfile("dataset/base_set/"+row[1], task+"validate/"+sample+"/"+(row[1].replace("/","_")))
 
 #task 3
-task = "task3/"
+task = "task3/c/"
 # Delete old files
 if os.path.exists(task) and os.path.isdir(task):
     shutil.rmtree(task)
